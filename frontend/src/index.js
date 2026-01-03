754c7e939189af5c956554f517fd134ff0ac92aa
Mehmetmrtgl <mehmetmrt1999@gmail.com>
2026-01-04 01:07:18 +0300

    style: update font to sans-serif

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,9 @@
-/*import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-*/
-
-// src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";  // 👈 İŞTE EKSİK OLAN KRİTİK SATIR BU!
 import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(<App />);
-
-/*
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";  // EKLENDİ
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-*/
